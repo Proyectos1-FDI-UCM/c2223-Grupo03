f7df9fc317ce9ddb9ae132f9882e82a74f2c9a73
miguelNPB <miguelnp@ucm.es>
2023-02-22 11:45:29 +0100

    fix daniel

---
+++ Assets/Scripts/MovementComponent.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementComponent : MonoBehaviour
+{
+    public float speed;
+
+    private Rigidbody2D _playerRigidbody;
+    private Vector2 movement;
+    private Animator _animator;
+
+
+    public void ChangeValues(float horizontal, float vertical)
+    {
+        movement = new Vector2(horizontal, vertical);
+    }
+
+    private void Update()
+    {
+        _animator.SetFloat("Horizontal", movement.x);
+        _animator.SetFloat("Vertical", movement.y);
+    }
+    private void Start()
+    {
+        movement = Vector2.zero;
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+    }
+    private void FixedUpdate()
+    {
+        _playerRigidbody.velocity = movement * speed;
+    }
+}
