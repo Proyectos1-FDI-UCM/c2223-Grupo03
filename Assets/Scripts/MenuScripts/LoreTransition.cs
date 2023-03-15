738bbbf71832a82f05904fe467ace271d1598073
Alvelex <125447396+AlvaPina@users.noreply.github.com>
2023-03-15 22:54:44 +0100

    LoreTransition (Queda Pulir)
    La hsitoria del Juego al empezar, contiene el texto animado y la transición de las imagenes, falta animación de imagenes y las imagenes. También me gustaría añadirle sonidos cada vez que escribe una letra.

--- Assets/Scripts/MenuScripts/LoreTransition.cs
+++ Assets/Scripts/MenuScripts/LoreTransition.cs
@@ -2,28 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoreTransition : MonoBehaviour
 {
     [SerializeField] int _lettersPerSecond;
     [SerializeField] TextMeshProUGUI _dialogText;
-    GameObject[] _transitions;
-    int _actualTransition = 0;
-    
+    private Image[] _transitions;
+    [SerializeField] GameObject _imageContainer;
+    [SerializeField] string[] _dialogs;
+
     void Start()
     {
-        _transitions = gameObject.GetComponentsInChildren<GameObject>();
-        
-        StartCoroutine(TypeDialog("test"));
+        _transitions = _imageContainer.GetComponentsInChildren<Image>(true);
+        Debug.Log(_transitions.Length);
+
+        StartCoroutine(TypeDialog(_dialogs));
     }
 
-    public IEnumerator TypeDialog(string dialog)
+    public IEnumerator TypeDialog(string[] dialogs)
     {
-        _dialogText.text = "";
-        foreach(var letter in dialog.ToCharArray())
+        for (int i = 0; i < _transitions.Length; i++)
         {
-            _dialogText.text += letter;
-            yield return new WaitForSeconds(1f / _lettersPerSecond);
+            // Mostrar la imagen correspondiente
+            _transitions[i].gameObject.SetActive(true);
+
+            _dialogText.text = "";
+            foreach (var letter in dialogs[i].ToCharArray())
+            {
+                _dialogText.text += letter;
+                yield return new WaitForSeconds(1f / _lettersPerSecond);
+            }
+
+            // Esperar un tiempo antes de continuar
+            yield return new WaitForSeconds(2f);
+
+            // Ocultar la imagen
+            _transitions[i].gameObject.SetActive(false);
         }
     }
 }
