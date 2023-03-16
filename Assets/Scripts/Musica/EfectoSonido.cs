aa04a1ab0d678615a2281471d79464b2f1c9b715
danzhu26x <dzhu@ucm.es>
2023-03-16 19:26:38 +0100

    ajustes de sonido

--- Assets/Scripts/Musica/EfectoSonido.cs
+++ Assets/Scripts/Musica/EfectoSonido.cs
@@ -7,11 +7,7 @@ public class EfectoSonido : MonoBehaviour
     [SerializeField] private AudioClip _ambiente;
     [SerializeField] private AudioClip _lluvia;
     // Start is called before the first frame update
-    void Start()
-    {
-        SoundController.Instance.PlayMusic(_ambiente);
-        SoundController.Instance.PlayMusic(_lluvia);    
-    }
+
 
     // Update is called once per frame
     void Update()
