b6431fbef906b1a4fa0ef8068f87c8f6df32ea78
miguelNPB <miguelnp@ucm.es>
2023-03-21 22:37:33 +0100

    arreglado killcopmonetn

--- Assets/Scripts/KillComponent.cs
+++ Assets/Scripts/KillComponent.cs
@@ -8,18 +8,26 @@ public class KillComponent : MonoBehaviour
 {
 
     #region References
+    [SerializeField] private GameObject _playerInCloset;
+    private EnemyAI _enemyAI;
     private RespawnComponent _respawnComp;
     #endregion
 
-    // Start is called before the first frame update
+    #region Methods
     void Start()
     {
+        _enemyAI = GetComponent<EnemyAI>();
         _respawnComp = GameManager.Instance.GetComponent<RespawnComponent>();
     }
 
-
-    #region Methods
-
+    private void Update()
+    {
+        if (_enemyAI.Chasing && _playerInCloset.active &&
+            Vector2.Distance(transform.position, _playerInCloset.transform.position) < 1f)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "Player")
