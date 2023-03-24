01c5c4515592fe5c37627d85ceed7de938fb3e21
Usuario Local <miguelnp@ucm.es>
2023-03-24 11:43:19 +0100

    level2

---
+++ Assets/Scripts/PlaySound.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    private AudioSource _audio;
+    public void PlayAudio()
+    {
+
+    }
+}
