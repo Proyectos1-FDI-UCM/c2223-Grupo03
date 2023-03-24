b211d9f710b9044cb4776274e3f33a6d9b38d8ab
miguelNPB <miguelnp@ucm.es>
2023-03-24 20:02:06 +0100

    mergeando con level selector

---
+++ Assets/Scripts/MenuScripts/SceneManagerScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void LevelSelector()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void CurrentSceneMove(int i)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + i);
+    }
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    public void LoadLevel(int lvl)
+    {
+        SceneManager.LoadScene(lvl);
+    }
+
+}
