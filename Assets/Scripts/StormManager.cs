e072d6e5dcc54a8ee1c4f69ef8b9c3ecb4c80dde
Usuario Local <alvapina@ucm.es>
2023-03-29 10:57:44 +0200

    Rayos sin terminar
    Hice que cada cierto tiempo entre dos intervalos se reproduce la animación de luz de los rayos y un ratito después el sonido

---
+++ Assets/Scripts/StormManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class StormManager : MonoBehaviour
+{
+    #region parameters
+    float cont = 0;
+    float rdnNum = 5;
+    #endregion
+    #region properties
+    AudioSource _audioSource;
+    #endregion
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        //desactiva todas las luces hijas del gameObject
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
+    }
+    void Update()
+    {
+        cont += Time.deltaTime;
+        Debug.Log("Cont: " + cont);
+        Debug.Log("RdnNum: " + rdnNum);
+        if (cont > rdnNum)
+        {
+            Debug.Log("Suena");
+            _audioSource.Play();
+            cont = 0;
+            rdnNum = GeneraRadom();
+        }
+        else if (cont > rdnNum - 0.4)
+        {
+            ActivarHijos(true);
+        }
+        else if (cont > rdnNum - 0.5)
+        {
+            ActivarHijos(false);
+        }
+        else if (cont > rdnNum - 0.6)
+        {
+            ActivarHijos(true);
+        }
+        else if (cont > rdnNum - 0.8)
+        {
+            ActivarHijos(false);
+        }
+        else if (cont > rdnNum - 1)
+        {
+            ActivarHijos(false);
+        }
+    }
+
+    static int GeneraRadom()
+    {
+        int num = Random.Range(5, 10);
+        return num;
+    }
+
+    void ActivarHijos(bool activar)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).gameObject.SetActive(activar);
+        }
+    }
+}
