2e04404f1f4bb3fcea50e10b1356466dded29f33
DanielZhu26 <dzhu@ucm.es>
2023-03-29 10:02:09 +0200

    Añadido SpawnAt para poner al jugador en el lugar correcto (Daniel y Pablo)

---
+++ Assets/Scripts/Checkpoints/SpawnAtComponent.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnAtComponent : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!GameManager.Instance.getSpawn.getCP)
+        {
+            GameManager.Player.transform.position = transform.GetChild(0).transform.position;
+        }
+        else
+        {
+            GameManager.Player.transform.position = transform.GetChild(1).transform.position;
+        }
+    }
+}
